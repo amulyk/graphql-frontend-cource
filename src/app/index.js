a82e5222a06ad37ea9e6522f63d6b4d51b19b9af
Andrii Mulyk <amulyk@lectrum.io>
2020-01-12 19:56:24 +0200

    add files

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,14 @@
+// Core
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// Other
+import { client } from './init/client';
 
 export const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <h1>App</h1>
-    </>
+    </ApolloProvider>
   )
 };
