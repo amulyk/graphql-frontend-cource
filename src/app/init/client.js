e8304a67ac12ca279da29964e4eabe4f6dfd3dc0
Andrii Mulyk <amulyk@lectrum.io>
2020-01-19 19:53:24 +0200

    add files

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,9 +1,18 @@
 // Core
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // GraphQL Server
 const uri = 'https://funded-pet-library.moonhighway.com/';
+const link = createHttpLink({
+  uri
+});
+
+// Cache initialization
+const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  uri
+  cache,
+  link
 });
